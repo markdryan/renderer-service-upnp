9aa4466a65ee7699f46f07d361f303e9050e7651
Ludovic Ferrandis <ludovic.ferrandis@intel.com>
2012-07-18 12:00:32 +0200

    [Autotools] Include config.h in Makefile.am
    Remove include file config.h from source files
    
    Signed-off-by: Ludovic Ferrandis <ludovic.ferrandis@intel.com>

--- src/error.c
+++ src/error.c
@@ -20,9 +20,6 @@
  *
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
 
 #include <gio/gio.h>
 #include <glib.h>
--- src/log.c
+++ src/log.c
@@ -20,9 +20,6 @@
  *
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
 
 #define _GNU_SOURCE
 #include <stdarg.h>
